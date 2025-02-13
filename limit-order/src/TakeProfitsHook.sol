98673fc2a42a32370e2c70b7e331f6fe3694e250
Architsharma7 <architsharma7911@gmail.com>
2025-02-13 15:44:40 +0530

    fix: added settle and redeem helper functions

--- limit-order/src/TakeProfitsHook.sol
+++ limit-order/src/TakeProfitsHook.sol
@@ -30,6 +30,8 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
     constructor(IPoolManager _manager) BaseHook(_manager) ERC1155("") {}
 
     error NotEnoughTokens();
+    error NothingToClaim();
+    error NotEnoughToClaim();
 
     function getHookPermissions()
         public
@@ -112,6 +114,100 @@ contract TakeProfitsHook is BaseHook, ERC1155 {
         _burn(msg.sender, positionId, amountToCancel);
         Currency token = zeroForOne ? key.currency0 : key.currency1;
         token.transfer(msg.sender, amountToCancel);
+    }
+
+    function redeem(
+        PoolKey calldata key,
+        int24 tickToSellAt,
+        bool zeroForOne,
+        uint256 inputAmountToClaimFor
+    ) external {
+        int24 tick = getLowerUsableTick(tickToSellAt, key.tickSpacing);
+        uint256 positionId = getPositionId(key, tick, zeroForOne);
+
+        if (claimableOutputTokens[positionId] == 0) revert NothingToClaim();
+
+        uint256 positionTokens = balanceOf(msg.sender, positionId);
+        if (positionTokens < inputAmountToClaimFor) revert NotEnoughToClaim();
+
+        uint256 totalClaimableForPosition = claimableOutputTokens[positionId];
+        uint256 totalInputAmountForPosition = claimTokensSupply[positionId];
+        uint256 outputAmount = inputAmountToClaimFor.mulDivDown(
+            totalClaimableForPosition,
+            totalInputAmountForPosition
+        );
+        claimableOutputTokens[positionId] -= outputAmount;
+        claimTokensSupply[positionId] -= inputAmountToClaimFor;
+        _burn(msg.sender, positionId, inputAmountToClaimFor);
+
+        Currency token = zeroForOne ? key.currency1 : key.currency0;
+        token.transfer(msg.sender, outputAmount);
+    }
+
+    function executeOrder(
+        PoolKey calldata key,
+        int24 tick,
+        bool zeroForOne,
+        uint256 inputAmount
+    ) internal {
+        BalanceDelta delta = swapAndSettleBalances(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: zeroForOne,
+                // provide a negative value here to signify an "exact input for output" swap
+                amountSpecified: -int256(inputAmount),
+                // No slippage limits (maximum slippage possible)
+                sqrtPriceLimitX96: zeroForOne
+                    ? TickMath.MIN_SQRT_PRICE + 1
+                    : TickMath.MAX_SQRT_PRICE - 1
+            })
+        );
+        pendingOrders[key.toId()][tick][zeroForOne] -= inputAmount;
+        uint256 positionId = getPositionId(key, tick, zeroForOne);
+        uint256 outputAmount = zeroForOne
+            ? uint256(int256(delta.amount1()))
+            : uint256(int256(delta.amount0()));
+        claimableOutputTokens[positionId] += outputAmount;
+    }
+
+    function swapAndSettleBalances(
+        PoolKey calldata key,
+        IPoolManager.SwapParams memory params
+    ) internal returns (BalanceDelta) {
+        BalanceDelta delta = poolManager.swap(key, params, "");
+        if (params.zeroForOne) {
+            // Negative Value => Money leaving user's wallet
+            // Settle with PoolManager
+            if (delta.amount0() < 0) {
+                _settle(key.currency0, uint128(-delta.amount0()));
+            }
+
+            // Positive Value => Money coming into user's wallet
+            // Take from PM
+            if (delta.amount1() > 0) {
+                _take(key.currency1, uint128(delta.amount1()));
+            }
+        } else {
+            if (delta.amount1() < 0) {
+                _settle(key.currency1, uint128(-delta.amount1()));
+            }
+
+            if (delta.amount0() > 0) {
+                _take(key.currency0, uint128(delta.amount0()));
+            }
+        }
+
+        return delta;
+    }
+
+    function _settle(Currency currency, uint128 amount) internal {
+        poolManager.sync(currency);
+        currency.transfer(address(poolManager), amount);
+        poolManager.settle();
+    }
+
+    function _take(Currency currency, uint128 amount) internal {
+        poolManager.take(currency, address(this), amount);
     }
 
     function getLowerUsableTick(
